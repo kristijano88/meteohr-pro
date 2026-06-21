7001b465ace5b4f820df4ad571bcc2fe8c2b16a0
kristijano88 <kristijanosman@gmail.com>
2026-06-21 18:36:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -83,10 +83,12 @@ for(let i=0;i<24;i++){
 
   h+=`
   <tr>
-    <td>${ico}</td>
-    <td>${d.hourly.time[i].slice(11,16)}</td>
-    <td>${d.hourly.temperature_2m[i]}°C</td>
-    <td>${rain}%</td>
+    <th></th>
+<th>Sat</th>
+<th>Temp</th>
+<th>Kiša</th>
+<th>Vjetar</th>
+<th>Udari</th>
   </tr>`;
 };
 hourly.innerHTML=h+"</table></div>";
